20740b9dfe14b5faa5f610ccdbc2b45f24fc86ca
Seline Wongso <mstp83@durham.ac.uk>
2025-06-08 23:13:15 +0100

    succesfully move player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,27 +7,38 @@ public class PlayerController : MonoBehaviour
     private PlayerInputActions _input;
     private CharacterController cc;
     private Vector2 movementVector;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 50f;
+
+    private System.Action<InputAction.CallbackContext> onMovePerformed;
+    private System.Action<InputAction.CallbackContext> onMoveCanceled;
+
 
     void Awake()
     {
         _input = new PlayerInputActions();
         cc = GetComponent<CharacterController>();
+
+        onMovePerformed = ctx => movementVector = ctx.ReadValue<Vector2>();
+    onMoveCanceled = ctx => movementVector = Vector2.zero;
+
     }
 
+    
     void OnEnable()
-    {
-        _input.PlayerActionMap.Enable();
-        _input.PlayerActionMap.Movement.performed += ctx => movementVector = ctx.ReadValue<Vector2>();
-        _input.PlayerActionMap.Movement.canceled  += ctx => movementVector = Vector2.zero;
-    }
+{
+    _input.PlayerActionMap.Enable();
+    _input.PlayerActionMap.Movement.performed += onMovePerformed;
+    _input.PlayerActionMap.Movement.canceled += onMoveCanceled;
+}
+
 
     void OnDisable()
-    {
-        _input.PlayerActionMap.Movement.performed -= ctx => movementVector = ctx.ReadValue<Vector2>();
-        _input.PlayerActionMap.Movement.canceled  -= ctx => movementVector = Vector2.zero;
-        _input.PlayerActionMap.Disable();
-    }
+{
+    _input.PlayerActionMap.Movement.performed -= onMovePerformed;
+    _input.PlayerActionMap.Movement.canceled -= onMoveCanceled;
+    _input.PlayerActionMap.Disable();
+}
+
 
     void Update()
     {
